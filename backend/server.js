2f6bc857bc9d45325f5f7c79b7db5a795d14173f
NB-famous <ncbadoles@gmail.com>
2021-05-23 14:06:04 -0400

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const nodemailer = require("nodemailer")
 const PORT = process.env.PORT || 5000;
 
 
+
+require('dotenv').config();
+
+
 //Middleware 
 
 app.use(express.static('frontend'))
@@ -20,14 +24,14 @@ app.post('/', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user:'' // Email here,
-            pass: '' //password for email here should be created in env file//
+            user:process.env.EMAIL, // Email here,
+            pass: process.env.PASS //password for email here should be created in env file//
         }
     })
 
     const mailOptions = {
         from: req.body.email,
-        to: "" // re-enter email here,
+        to: process.env.EMAIL, // re-enter email here,
         subject: `Message from ${req.body.email}: ${req.body.subject}`,
         text: req.body.message
     }
